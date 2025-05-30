ec0a96a038a6ef3578b81bd7c9c456b6dde46efa
Surjendu kar <rahulgotp9988@gmai.com>
2025-05-30 19:18:20 +0530

    Enhance Header component with improved styling and layout adjustments for search bar and cart button; added avatar placeholder.

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,26 +13,28 @@ function Header() {
 
       {/* Search Bar */}
         <div className="relative w-full max-w-md">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
+          <span className="absolute inset-y-0 left-0 flex items-center lg:pl-3 pl-2 text-gray-400">
             <Search size={20} />
           </span>
           <input
             type="text"
             placeholder="Search for products..."
-            className="w-full pl-10 pr-4 py-2 rounded border border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-400 bg-transparent text-white placeholder:text-blue-100"
+            className="w-full lg:pl-10 pl-8 pr-4 py-2 rounded border border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-400 bg-transparent text-white placeholder:text-blue-100"
           />
         </div>
       
-      <div>
+      <div className="flex items-center  gap-2 lg:gap-8">
         {/* Cart Button */}
         <Link href="/cart">
-          <button className="flex items-center gap-2 bg-[#152657] hover:bg-[#10204e] text-white font-semibold px-6 py-2 rounded-md transition" >
+          <button className="flex items-center lg:gap-2 bg-[#152657] hover:bg-[#10204e] text-white font-semibold lg:px-6 lg:py-2 p-3 lg:rounded-md rounded-full transition">
             <ShoppingCart size={20} />
-            Cart
+            <span className="hidden sm:inline">Cart</span>
           </button>
         </Link>
-        {/*  avatar */}
-        
+        {/* Avatar */}
+        <div className="w-9 h-9 rounded-full bg-blue-300 flex items-center justify-center text-blue-900 font-bold text-lg cursor-pointer">
+          A
+        </div>
       </div>
     </div>
   )
